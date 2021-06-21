6533373c6670c457404e5a27c8af8cea72d99f21
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-06-21 17:48:32 +0200

    add new file for models and export them

--- models.js
+++ models.js
@@ -1,1 +1,31 @@
 const mongoose = require("mongoose"); // require mongoose
+
+let movieSchema = mongoose.schema({
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
+  Genre: {
+    Name: String,
+    Description: String,
+  },
+  Director: {
+    Name: String,
+    Bio: String,
+  },
+  Actors: [String],
+  ImagePath: String,
+  Featured: Boolean,
+});
+
+let userSchema = mongoose.schema({
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
+  Birthday: Date,
+  FavouriteMovies: [{ type: mongoose.schema.Types.ObjectId, ref: "Movie" }],
+});
+
+let Movie = mongoose.model("Movie", movieSchema);
+let User = mongoose.model("User", userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
