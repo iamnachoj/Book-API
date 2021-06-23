75432e6e2a2fc9e5ab2f4cbc72c98c1e77e87664
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-06-23 15:56:03 +0200

    add 'GET users by name'

--- index.js
+++ index.js
@@ -54,6 +54,18 @@ app.get("/users", (req, res) => {
     });
 });
 
+// GET a user by username
+app.get("/users/:Username", (req, res) => {
+  Users.findOne({ Name: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
+
 // POST requests
 app.post("/register", (req, res) => {
   Users.findOne({ $or: [{ Name: req.body.Name }, { Email: req.body.Email }] })
