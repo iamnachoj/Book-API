4e5d4ea444a72b8f6bdbbb56e6955c44f2a429d5
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-06-30 16:18:43 +0200

    install and code Cross Origin Resource Sharing

--- index.js
+++ index.js
@@ -24,6 +24,26 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
 }); // deal with error
+
+//Cross-origin resource sharing
+const cors = require("cors");
+let allowedOrigins = ["http://localhost:8080"];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          "The CORS policy for this application doesn’t allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+); //end of CORS
+
 let auth = require("./auth")(app); // it is placed here because it needs to be AFTER body parser is called.
 
 // GET homepage
