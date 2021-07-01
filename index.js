47ea0dc484b5abacff729a8a64064e5164ef42ac
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-07-01 13:58:48 +0200

    add user authentication for /users endpoint

--- index.js
+++ index.js
@@ -85,28 +85,36 @@ app.get(
 );
 
 // Get all users
-app.get("/users", (req, res) => {
-  Users.find()
-    .then((users) => {
-      res.status(201).json(users);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+app.get(
+  "/users",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Users.find()
+      .then((users) => {
+        res.status(201).json(users);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 // GET a user by username
-app.get("/users/:Username", (req, res) => {
-  Users.findOne({ Name: req.params.Username })
-    .then((user) => {
-      res.json(user);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+app.get(
+  "/users/:Username",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Users.findOne({ Name: req.params.Username })
+      .then((user) => {
+        res.json(user);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 // POST register a new user
 app.post(
@@ -201,16 +209,6 @@ app.delete("/users/:Username/Movies/:MovieID", (req, res) => {
 
 // PUT to update a user's info, by username
 app.put("/users/:Name", (req, res) => {
-  /* We’ll expect JSON in this format
-  {
-    Username: String,
-    (required)
-    Password: String,
-    (required)
-    Email: String,
-    (required)
-    Birthday: Date
-  }*/
   Users.findOneAndUpdate(
     { Name: req.params.Name },
     {
