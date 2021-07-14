730d5bfa7e504f6ce68297090e3a722ff4831a1e
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-07-14 18:34:42 +0200

    remove conflicting files from main: these are part of the change that is happening on 'restructuring branch'

--- src/api/v1/user/index.js
+++
@@ -1,8 +0,0 @@
-const {Router} = require('express')
-const userController = require('./user.controller')
-
-const userRouter = Router();
-
-userRouter.route('/register').post(userController.registerUser);
-
-module.exports = userRouter;
