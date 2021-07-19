5e913f09e0bcea977c4f0ab3bd915211b2a9d77e
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-07-19 16:54:49 +0200

    add middleware to it's right folder, re-organised

---
+++ src/middlewares/index.js
@@ -0,0 +1,18 @@
+const morgan = require("morgan");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const initVendorMiddlewares = (app) => {
+  app.use(cors(),);
+  app.use(morgan("common"));
+  app.use(bodyParser.json({ limit: '300kb' }));
+  app.use(
+    bodyParser.urlencoded({
+      extended: true,
+      limit: '300kb',
+    }),
+  );
+  app.use(express.static("public"));
+};
+
+module.exports = {initVendorMiddlewares}
